63487a6f5d9da08568289a731cca8404a0b9956a
kiyahassan666--create <kiyahassan666@gmail.com>
2026-07-25 12:00:45 +0300

    Hardcode Firebase config directly in lib/firebase.ts to eliminate env var substitution issues on Vercel

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore, Firestore } from "firebase/firestore";
 import { getStorage, FirebaseStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyBS7z71Z886VuXTySpmxrC3Hf30gkNUCzE",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "dunia-boutique.firebaseapp.com",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "dunia-boutique",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "dunia-boutique.firebasestorage.app",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "842999784659",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:842999784659:web:37e2b6dbe988d89c362026",
+  apiKey: "AIzaSyBS7z71Z886VuXTySpmxrC3Hf30gkNUCzE",
+  authDomain: "dunia-boutique.firebaseapp.com",
+  projectId: "dunia-boutique",
+  storageBucket: "dunia-boutique.firebasestorage.app",
+  messagingSenderId: "842999784659",
+  appId: "1:842999784659:web:37e2b6dbe988d89c362026",
 };
 
 console.log("[Firebase] Config loaded:", {
